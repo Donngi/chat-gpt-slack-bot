423727ae75287ba76e9ace450d1648f6f781877e
Jimon-s <22317839+Jimon-s@users.noreply.github.com>
2023-03-24 06:46:15 +0900

    Add build script

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
   extends: ["prettier"],
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2020,
+  },
 };
---
+++ package/lambda_gateway/scripts/build.mjs
@@ -0,0 +1,36 @@
+import * as esbuild from "esbuild";
+import pkg from "../package.json" assert { type: "json" };
+import { execSync } from "child_process";
+import { writeFileSync } from "fs";
+
+const unbundleModules = ["@slack/bolt"];
+
+// From package root.
+const outdir = "./dist";
+const packageLockJSONPath = "../../package-lock.json";
+
+await esbuild.build({
+  entryPoints: ["src/index.ts"],
+  outdir: outdir,
+  platform: "node",
+  format: "esm",
+  minify: true,
+  sourcemap: true,
+  bundle: true,
+  external: unbundleModules,
+});
+
+const extractedDependencies = Object.keys(pkg.dependencies)
+  .filter((key) => unbundleModules.includes(key))
+  .reduce((obj, key) => {
+    obj[key] = pkg.dependencies[key];
+    return obj;
+  }, {});
+
+const extractedPackageJSON = {
+  dependencies: extractedDependencies,
+};
+
+writeFileSync(`${outdir}/package.json`, JSON.stringify(extractedPackageJSON));
+execSync(`cp ${packageLockJSONPath} ${outdir}`);
+execSync(`cd ${outdir} && npm ci --production`);
